063e5f0b36557b2dc893d79ec5657866f2631ebd
will <will@willlam.net>
2016-01-12 17:46:11 -0500

    Initial Commit

---
+++ contactlist/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  contactlist
+//
+//  Created by William Lam on 2016-01-12.
+//  Copyright © 2016 William Lam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+    return 0;
+}
