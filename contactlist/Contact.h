b018f42da1d0d2236bedf3bffebeceb26ec91ff5
will <will@willlam.net>
2016-01-12 20:42:41 -0500

    got menu to work because of mistakenly calling the CLASS method =/ got exit functionality to work tho

--- contactlist/Contact.h
+++ contactlist/Contact.h
@@ -9,5 +9,5 @@
 #import <Foundation/Foundation.h>
 
 @interface Contact : NSObject
-
+//- (instancetype)initW
 @end
--- contactlist/main.m
+++ contactlist/main.m
@@ -16,30 +16,18 @@
     ContactList *contactList = [[ContactList alloc] init];
     
     while (YES) {
-      
+
+
       NSString *menuString = @"What would you like to do next?\nnew - Create a new contact list\nlist - List all contacts\nquit - Exit Application";
       
       
       InputCollector *inputCollector = [[InputCollector alloc] init];
       
-      NSString *userSelection = [InputCollector inputForPrompt:menuString];
-//
-//      if ([userSelection isEqual:@"quit"]) {
-//        return 0;
-//      }
-//      if ([userSelection isEqual:@"new"]) {
-//        NSString *contactName = [userInput inputForPrompt:@"Enter a new contact name: "];
-//        NSString *contactEmail = [userInput inputForPrompt:@"Enter contact email: "];
-//        Contact *contact = [[Contact alloc] init];
-//        
-//        contact.name = contactName;
-//        contact.email = contactEmail;
-//        
-//        [contactList addContact:contact];
-//      }
-//      if ([userSelection isEqualTo:@"list"]) {
-//        [contactList printContactList];
-//      }
+      NSString *userSelection = [inputCollector inputForPrompt:menuString];
+
+      if ([userSelection isEqual:@"quit"]) {
+        return 0;
+      }
     }
     
     return 0;
